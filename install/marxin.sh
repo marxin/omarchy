c888e94f580a4ed7473f35e3f1751e7d18884624
Martin Liska <martin.liska@hey.com>
2025-07-12 20:09:54 +0200

    Install postgresql

--- install/marxin.sh
+++ install/marxin.sh
@@ -4,7 +4,8 @@ yay -R --noconfirm rust chromium
 yay -S --noconfirm --needed iftop iotop htop autoconf automake \
     make cmake gcc clang lld mold qemu elfutils gdb valgrind strace bloaty wild \
     rustup google-chrome cargo-binstall cargo-nextest samply \
-    mono mono-msbuild mono-nuget nodejs npm ipython
+    mono mono-msbuild mono-nuget nodejs npm ipython visual-studio-code-bin \
+    postgresql
 
 rustup toolchain install nightly
 
