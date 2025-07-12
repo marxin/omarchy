42a9349998bd8d6ab4a7011112b598d839252cd0
Martin Liska <martin.liska@hey.com>
2025-07-12 21:07:16 +0200

    Install atuin and starship properly

--- install/marxin.sh
+++ install/marxin.sh
@@ -5,7 +5,7 @@ yay -S --noconfirm --needed iftop iotop htop autoconf automake \
     make cmake gcc clang lld mold qemu elfutils gdb valgrind strace bloaty wild \
     rustup google-chrome cargo-binstall cargo-nextest samply \
     mono mono-msbuild mono-nuget nodejs npm ipython visual-studio-code-bin \
-    postgresql perf atuin
+    postgresql perf atuin starship
 
 rustup toolchain install nightly
 
