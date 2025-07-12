09f0f9cb26ed21ab7877b563e64f0d8311c8da04
Martin Liska <martin.liska@hey.com>
2025-07-12 19:17:36 +0200

    Install more deps

--- install/marxin.sh
+++ install/marxin.sh
@@ -3,6 +3,9 @@ yay -R --noconfirm rust chromium
 # TODO: poop: bad version of zig
 yay -S --noconfirm --needed iftop iotop htop autoconf automake \
     make cmake gcc clang lld mold qemu elfutils gdb valgrind strace bloaty wild \
-    rustup google-chrome cargo-binstall cargo-nextest samply
+    rustup google-chrome cargo-binstall cargo-nextest samply \
+    mono mono-msbuild mono-nuget nodejs npm ipython
 
 rustup toolchain install nightly
+
+sudo npm install --global coffeescript less
