b48e9c05d206bf83e5697467be5d9fcdb17cd095
Martin Liska <martin.liska@hey.com>
2025-07-12 20:32:06 +0200

    Install and use atuin for history

--- install/marxin.sh
+++ install/marxin.sh
@@ -5,7 +5,7 @@ yay -S --noconfirm --needed iftop iotop htop autoconf automake \
     make cmake gcc clang lld mold qemu elfutils gdb valgrind strace bloaty wild \
     rustup google-chrome cargo-binstall cargo-nextest samply \
     mono mono-msbuild mono-nuget nodejs npm ipython visual-studio-code-bin \
-    postgresql
+    postgresql perf atuin
 
 rustup toolchain install nightly
 
