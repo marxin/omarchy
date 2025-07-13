09b933106cac242dff37dc9614d39e845f5e306b
Martin Liska <martin.liska@hey.com>
2025-07-13 21:44:54 +0200

    Enhance install deps script

--- install/marxin.sh
+++ install/marxin.sh
@@ -2,12 +2,18 @@ yay -R --noconfirm rust chromium
 
 # TODO: poop: bad version of zig
 yay -S --noconfirm --needed iftop iotop htop autoconf automake \
-    make cmake gcc clang lld mold qemu elfutils gdb valgrind strace bloaty wild \
+    make cmake gcc aarch64-linux-gnu-gcc riscv64-linux-gnu-gcc clang lld mold \
+    qemu qemu-user elfutils gdb valgrind strace bloaty wild \
     rustup google-chrome cargo-binstall cargo-nextest samply \
     mono mono-msbuild mono-nuget nodejs npm ipython visual-studio-code-bin \
-    postgresql perf atuin starship
+    postgresql perf atuin starship ninja
 
 rustup toolchain install nightly
+rustup target add --toolchain nightly \
+    x86_64-unknown-linux-musl \
+    aarch64-unknown-linux-gnu \
+    riscv64gc-unknown-linux-gnu
+rustup component add rustc-codegen-cranelift-preview --toolchain nightly
 rustup default nightly
 
 sudo npm install --global coffeescript less
