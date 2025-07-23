4251016fe647367924a85f449df9e087f413c02e
Martin Liska <martin.liska@hey.com>
2025-07-23 06:43:13 +0200

    Install hyperfine

--- install/marxin.sh
+++ install/marxin.sh
@@ -7,7 +7,7 @@ yay -S --noconfirm --needed iftop iotop htop autoconf automake \
     qemu qemu-user elfutils gdb valgrind strace bloaty wild \
     rustup google-chrome cargo-binstall cargo-nextest samply \
     mono mono-msbuild mono-nuget nodejs npm ipython visual-studio-code-bin \
-    postgresql perf atuin starship ninja uutils-coreutils thunderbird
+    postgresql perf atuin starship ninja uutils-coreutils thunderbird hyperfine
 
 rustup toolchain install nightly
 rustup target add --toolchain nightly \
