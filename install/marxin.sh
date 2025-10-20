c790c8f62451ffac69f08455b3772bf2536fd742
Martin Liska <martin.liska@hey.com>
2025-10-20 20:57:38 +0200

    marxin.sh: move to pacman

--- install/marxin.sh
+++ install/marxin.sh
@@ -1,12 +1,13 @@
 # TODO: poop: bad version of zig: can be just downloaded
 
-yay -S --noconfirm --needed iftop iotop htop autoconf automake \
+sudo pacman -Sy iftop iotop htop autoconf automake \
   make cmake gcc aarch64-linux-gnu-gcc riscv64-linux-gnu-gcc clang lld mold \
   qemu qemu-user elfutils gdb valgrind strace bloaty wild \
   rustup cargo-binstall cargo-nextest samply \
-  mono mono-msbuild mono-nuget nodejs npm ipython visual-studio-code-bin \
+  mono mono-msbuild nodejs npm ipython visual-studio-code-bin \
   postgresql perf atuin starship ninja uutils-coreutils thunderbird hyperfine \
   rsync wasmer wabt binaryen
+yay -S mono-nugget
 
 rustup toolchain install nightly
 rustup target add --toolchain nightly \
