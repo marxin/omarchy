077582bc666b63ffca7c479baf7468af7727122d
Martin Liska <martin.liska@hey.com>
2025-10-20 19:25:16 +0200

    start using chromium

--- install/marxin.sh
+++ install/marxin.sh
@@ -1,11 +1,9 @@
-yay -R --noconfirm rust chromium
-
 # TODO: poop: bad version of zig: can be just downloaded
 
 yay -S --noconfirm --needed iftop iotop htop autoconf automake \
   make cmake gcc aarch64-linux-gnu-gcc riscv64-linux-gnu-gcc clang lld mold \
   qemu qemu-user elfutils gdb valgrind strace bloaty wild \
-  rustup google-chrome cargo-binstall cargo-nextest samply \
+  rustup cargo-binstall cargo-nextest samply \
   mono mono-msbuild mono-nuget nodejs npm ipython visual-studio-code-bin \
   postgresql perf atuin starship ninja uutils-coreutils thunderbird hyperfine \
   wasmer wabt binaryen
