a021488272147d1cb973fee775234eaee55df1fe
Martin Liska <martin.liska@hey.com>
2025-10-21 21:17:38 +0200

    fix nuget packages installation

--- install/marxin.sh
+++ install/marxin.sh
@@ -4,10 +4,9 @@ sudo pacman -Sy iftop iotop htop autoconf automake \
   make cmake gcc aarch64-linux-gnu-gcc riscv64-linux-gnu-gcc clang lld mold \
   qemu qemu-user elfutils gdb valgrind strace bloaty wild \
   rustup cargo-binstall cargo-nextest samply \
-  mono mono-msbuild nodejs npm ipython visual-studio-code-bin \
+  mono mono-msbuild nuget nodejs npm ipython visual-studio-code-bin \
   postgresql perf atuin starship ninja uutils-coreutils thunderbird hyperfine \
   rsync wasmer wabt binaryen
-yay -S mono-nugget
 
 rustup toolchain install nightly
 rustup target add --toolchain nightly \
