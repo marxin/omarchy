eef2d2d13932af971d552e590fc08d5baa6d1bb3
Martin Liska <martin.liska@hey.com>
2025-10-23 22:20:41 +0200

    add new dependency

--- install/marxin.sh
+++ install/marxin.sh
@@ -6,7 +6,8 @@ sudo pacman -Sy iftop iotop htop autoconf automake \
   rustup cargo-binstall cargo-nextest samply \
   mono mono-msbuild nuget nodejs npm ipython visual-studio-code-bin \
   postgresql perf atuin starship ninja uutils-coreutils thunderbird hyperfine \
-  rsync wasmer wabt binaryen
+  rsync wasmer wabt binaryen webkit2gtk
+yay -S mono-nugget
 
 rustup toolchain install nightly
 rustup target add --toolchain nightly \
