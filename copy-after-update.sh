155ab72f7c1a01a8696174751bcad9bbc466cba9
Martin Liska <martin.liska@hey.com>
2025-10-24 11:43:57 +0200

    Add utility installation script

---
+++ copy-after-update.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+cp config/hypr/bindings.conf ~/.config/hypr/
+cp config/hypr/input.conf ~/.config/hypr/
+cp config/waybar/config.jsonc ~/.config/waybar
+cp config/waybar/style.css ~/.config/waybar
+cp default/bash/* ~/.local/share/omarchy/default/bash
+cp config/uwsm/default ~/.config/uwsm/default
