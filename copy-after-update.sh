a2f68ce10c1f686638abc58042da89743330d4a7
Martin Liska <martin.liska@hey.com>
2026-01-09 08:58:30 +0100

    include cargo config file

--- copy-after-update.sh
+++ copy-after-update.sh
@@ -7,6 +7,7 @@ cp config/waybar/style.css ~/.config/waybar
 cp default/bash/* ~/.local/share/omarchy/default/bash
 cp config/uwsm/default ~/.config/uwsm/default
 cp config/starship.toml ~/.config/
+cp config/cargo/config.toml ~/.cargo/
 cp bin/omarchy-menu ~/.local/share/omarchy/bin/
 
 cp default-configs/.* ~
