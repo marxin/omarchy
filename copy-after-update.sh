3e204e851c730301ee869b240ca8e6ad5fcc864d
Martin Liska <martin.liska@hey.com>
2026-02-28 07:42:40 +0100

    fix copying

--- copy-after-update.sh
+++ copy-after-update.sh
@@ -4,7 +4,7 @@ cp config/hypr/bindings.conf ~/.config/hypr/
 cp config/hypr/input.conf ~/.config/hypr/
 cp config/waybar/config.jsonc ~/.config/waybar
 cp config/waybar/style.css ~/.config/waybar
-cp default/bash/* ~/.local/share/omarchy/default/bash
+cp -r default/bash/* ~/.local/share/omarchy/default/bash
 cp config/uwsm/default ~/.config/uwsm/default
 cp config/starship.toml ~/.config/
 cp config/cargo/config.toml ~/.cargo/
