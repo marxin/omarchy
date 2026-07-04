d01a2e5d3b1f604186dc963c0056d583050c387b
Martin Liska <martin.liska@hey.com>
2026-07-04 09:11:25 +0200

    drop usage of webkit2gtk

--- install/marxin.sh
+++ install/marxin.sh
@@ -6,7 +6,7 @@ sudo pacman -Sy iftop iotop htop autoconf automake \
   rustup cargo-binstall cargo-nextest samply \
   mono mono-msbuild nuget nodejs npm ipython visual-studio-code-bin \
   postgresql perf atuin starship ninja uutils-coreutils thunderbird hyperfine \
-  rsync wasmer wabt binaryen webkit2gtk gitoxide wasm-tools wasmtime wasm-pack \
+  rsync wasmer wabt binaryen gitoxide wasm-tools wasmtime wasm-pack \
   bugstalker
 yay -S mono-nugget
 
